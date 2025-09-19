ead6f942c7e98fe23c6db1347d3de8dab3f8e5ab
qosain-bukhari <bukhariqosain824@gmail.com>
2025-09-19 18:42:01 +0500

    Data transformation and model training

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -61,3 +61,26 @@ class DataValidationConfig:
             trainingpipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
             trainingpipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
         )
+
+class DataTransformationConfig:
+     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,trainingpipeline.DATA_TRANSFORMATION_DIR_NAME )
+        self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,trainingpipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            trainingpipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,  trainingpipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            trainingpipeline.TEST_FILE_NAME.replace("csv", "npy"), )
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, trainingpipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            trainingpipeline.PREPROCESSING_OBJECT_FILE_NAME,)
+        
+    
+class ModelTrainerConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_trainer_dir: str = os.path.join(
+            training_pipeline_config.artifact_dir, trainingpipeline.MODEL_TRAINER_DIR_NAME
+        )
+        self.trained_model_file_path: str = os.path.join(
+            self.model_trainer_dir, trainingpipeline.MODEL_TRAINER_TRAINED_MODEL_DIR, 
+            trainingpipeline.MODEL_FILE_NAME
+        )
+        self.expected_accuracy: float = trainingpipeline.MODEL_TRAINER_EXPECTED_SCORE
+        self.overfitting_underfitting_threshold = trainingpipeline.MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD
